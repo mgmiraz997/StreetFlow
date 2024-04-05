f7d5140ca6cc7499ae3dd156f96fd09b89b303d8
Miraz6495 <hunter.miraz@gmail.com>
2024-04-05 16:21:30 +0600

    correction 005

--- StreetFlow/urls.py
+++ StreetFlow/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import (views)
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
 ]
