1024350bbb2bb1a25ebac6a9ac8f3120fea0f2bd
maharazaman0127 <maharazaman16@gmail.com>
2024-04-05 15:59:38 +0600

    css solved

--- StreetFlow/urls.py
+++ StreetFlow/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from .import (views)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('',views.style,name='style'),
 ]
