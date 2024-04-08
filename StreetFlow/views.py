33f9f55801fce1332f9af51052c99cec2d3189bf
Miraz6495 <hunter.miraz@gmail.com>
2024-04-08 13:42:17 +0600

    Login signup full

--- StreetFlow/views.py
+++ StreetFlow/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth.models import User, auth
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 
 def home(request):
@@ -9,17 +9,13 @@ def home(request):
         password = request.POST['password']
         data = User.objects.create_user(username=username, email=email, password=password)
         data.save()
-        return HttpResponse("sucessful")
-
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
         user = auth.authenticate(email=email, password=password)
-
         if user is not None:
             auth.login(request, user)
             return redirect('menu')
-
     return render(request, 'home.html')
 
 
