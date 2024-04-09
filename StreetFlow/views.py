d788e794e2896a126218129de82c2e2513cdaf19
jakariarafi <jakariarafi18@gmail.com>
2024-04-10 00:25:00 +0600

    regi

--- StreetFlow/views.py
+++ StreetFlow/views.py
@@ -1,25 +1,32 @@
 
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
+from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def home(request):
+    error = None
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        data = User.objects.create_user(username=username, email=email, password=password)
-        data.save()
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = auth.authenticate(email=email, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('menu')
-        else:
-            return redirect("Wrong Password")
-    return render(request, 'home.html')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Check if the user is trying to register
+        if 'register' in request.POST:
+            data = User.objects.create_user(username=username, email=email, password=password)
+            data.save()
+            error = "Registration successful! Please login."
+
+        # If the user is trying to log in
+        elif 'login' in request.POST:
+            user = auth.authenticate(email=email, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('menu')
+            else:
+                error = "Invalid email or password"
+
+    return render(request, 'home.html', {'error': error})
 
 
 def menu(request):
