f648f8744987ee81e45515637752e8ec9f143402
Miraz6495 <hunter.miraz@gmail.com>
2024-04-09 17:46:48 +0600

    Forget password error2

--- streetflow1/urls.py
+++ streetflow1/urls.py
@@ -1,12 +1,13 @@
 from django .urls import path
 from .import views
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
     path('', views.home, name='home'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
 
-    # path('logout/',auth_views.LoginView.as_view(),name='logout'),
+
 
 
 
@@ -15,8 +16,6 @@ urlpatterns = [
 
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
 
-    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confir m'),
-
-    # path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 
 ]
--- streetflow1/views.py
+++ streetflow1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
+
+def home(request):
+    pass
