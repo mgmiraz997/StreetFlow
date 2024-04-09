36f385c01cb80e489997932d9f7e551792a59d18
jakariarafi <jakariarafi18@gmail.com>
2024-04-10 00:04:19 +0600

    modified5

--- StreetFlow/views.py
+++ StreetFlow/views.py
@@ -1,31 +1,24 @@
-from django.contrib import auth
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 
 def home(request):
-    error = None
     if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        data = User.objects.create_user(username=username, email=email, password=password)
+        data.save()
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('menu')
 
-        # Check if the user is trying to register
-        if 'register' in request.POST:
-            data = User.objects.create_user(username=username, email=email, password=password)
-            data.save()
-            error = "Registration successful! Please login."
-
-        # If the user is trying to log in
-        elif 'login' in request.POST:
-            user = auth.authenticate(email=email, password=password)
-            if user is not None:
-                auth.login(request, user)
-                return redirect('menu')
-            else:
-                error = "Invalid email or password"
-
-    return render(request, 'home.html', {'error': error})
+    return render(request, 'home.html')
 
 
 def menu(request):
--- streetflow1/urls.py
+++ streetflow1/urls.py
@@ -16,6 +16,4 @@ urlpatterns = [
 
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
 
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-
 ]
