55c7f16393e9503f37c6884feb228e06dddd8f64
Miraz6495 <hunter.miraz@gmail.com>
2024-04-19 21:53:57 +0600

    Map 3

---
+++ map/management/commands/simulate_vehicle_movement.py
@@ -0,0 +1,22 @@
+import random
+import time
+from django.core.management.base import BaseCommand
+from map.models import Vehicle
+
+
+class Command(BaseCommand):
+    help = 'Simulate vehicle movement'
+
+    def handle(self, *args, **options):
+        # Define the range of latitude and longitude variation
+        let_lon_var = 0.1  # 0.1 degree is approximately 11km
+
+        while True:
+            print("Vehicle positions updating...")
+
+            for vehicle in Vehicle.objects.all():
+                vehicle.latitude = vehicle.latitude + random.uniform(-let_lon_var, let_lon_var)
+                vehicle.longitude = vehicle.longitude + random.uniform(-let_lon_var, let_lon_var)
+                vehicle.save()
+
+            time.sleep(5)
