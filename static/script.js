b893370b54c22ca30d07b61e59010072d8870201
Miraz6495 <hunter.miraz@gmail.com>
2024-04-20 20:58:40 +0600

    Map 7

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ function createMap(center, zoom) {
     let map = L.map('map').setView(center, zoom);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     return map
 }
