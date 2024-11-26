b1d953fbe12e57b73c7caa8606d0e21164f233ba
Miraz6495 <hunter.miraz@gmail.com>
2024-11-26 19:15:32 +0600

    Selenium test for login verification

---
+++ Selenium Reports/login.py
@@ -0,0 +1,34 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+@pytest.mark.parametrize("username, password", [
+    ("customer1@gmail.com", "123456"),
+    ("leodas@gmail.com", "123456"),
+    ("test", "test"),
+    ("admin", "admin"),
+
+])
+def test_login(driver, username, password):
+    driver.get("http://127.0.0.1:8000/")
+    username_field = driver.find_element(By.NAME, "email")
+    password_field = driver.find_element(By.NAME, "password")
+    submit_button = driver.find_element(By.CSS_SELECTOR, ".login-form .button > input")
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    submit_button.click()
+    assert "Successful" in driver.page_source
--- Selenium Reports/map_result.py
+++
@@ -1,6 +0,0 @@
-def map_result(x):
-    return x + 1
-
-
-def map_test():
-    assert map_result(1) == 3
